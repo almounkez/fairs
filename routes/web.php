27a79752725c49dda0d93b1f7e65bda786153b9b
Innosys <gitlab@almounkez.com>
2021-02-26 22:04:51 +0200

    step 022

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,24 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('locale/{locale}', function ($locale){
+    if (! in_array($locale, ['en', 'ar'])) {
+        abort(400);
+    }
+    Session::put('locale', $locale);
+    App::setLocale($locale);
+    return redirect()->back();
+});
+Route::get('/clear', function() {
 
+   Artisan::call('cache:clear');
+   Artisan::call('config:clear');
+   Artisan::call('config:cache');
+   Artisan::call('view:clear');
+
+   return "Cleared!";
+
+});
 Route::get('/', function () {
     return view('welcome');
 });
