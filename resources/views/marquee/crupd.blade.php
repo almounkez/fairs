cb7b66b2f280db6d5f1b7db51448804fd3bcab1f
rafah mnjd <rafahmnjd@gmail.com>
2021-03-05 11:56:32 +0200

    marquee Controller

--- resources/views/marquee/crupd.blade.php
+++ resources/views/marquee/crupd.blade.php
@@ -2,4 +2,70 @@
 
 @section('content')
 
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">{{ __('marquee') }}</div>
+            <div class="card-body">
+                <form action="{!!  !empty($marquee) ? route('marquee.update', $marquee) : route('marquee.store') !!}"
+                    method="Post" enctype="multipart/form-data">
+                    @csrf
+                    @if (!empty($marquee))
+                    @method('PUT')
+                    @endif
+
+                    @if (!empty($marquee))
+                    <div class="row">
+                        <div class="col-md-4 col-form-label " style="text-align:start">
+                            <label class="" for="id">{{ __('id') }} : </label>
+                        </div>
+                        <div class="col-8">
+                            {{ $marquee->id }}
+                        </div>
+                    </div>
+                    @elseif(!empty($fairId))
+                    <input name="fair_id" value="{{$fairId}}" hidden>
+                    @elseif(!empty($suiteId))
+                    <input name="suite_id" value="{{$suiteId}}" hidden>
+                    @endif
+                    <div class="row">
+                        <div class="col-md-2 col-form-label text-md-left">
+                            <label for="newstext_ar">{{ __('Arabic Text') }}:</label>
+                        </div>
+                        <div class="col-10">
+                            <textarea class="form-control" type="text" name="newstext_ar">
+                            @if (!empty($marquee) && old('newstext_ar', $marquee->newstext_ar)) {{ $marquee->newstext_ar }}@else{{old('newstext_ar')}} @endif
+                            </textarea>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row form-group">
+                        <div class="col-md-2 col-form-label text-md-left">
+                            <label for="newstext_en">{{ __('English Text') }}:</label>
+                        </div>
+                        <div class="col-10">
+                            <textarea class="form-control" type="text" name="newstext_en">
+                                @if (!empty($marquee) && old('newstext_en', $marquee->newstext_en))
+                                {{ $marquee->newstext_en }}
+                                @else{{old('newstext_en')}}
+                                 @endif
+                            </textarea>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-4 text-md-center">
+                            <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
+                        </div>
+                        <div class="col-8 text-md-center">
+                            <a href="{{ route('marquee.index') }}" type="button"
+                                class="btn btn-secondary">{{ __('Cancel') }}</a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
