e8164fa26d054bf11ea91d331eab969379513a0f
Innosys <gitlab@almounkez.com>
2021-03-05 17:17:23 +0200

    kim00332

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -6,10 +6,10 @@
         <div class="card">
             <div class="card-header">{{ __('Category') }}
 
-                <a href="{{ route('category.create',$fair->id) }}">
+                {{-- <a href="{{ route('category.create',$fair->id) }}"> --}}
                     <span class="iconify" data-icon="gridicons-add" data-inline="false" height="36" width="36">
                     </span>
-                </a>
+                {{-- </a> --}}
             </div>
 
             <div class="card-body">
@@ -18,38 +18,36 @@
                         <thead>
                             <tr>
                                 <th scope="col">#</th>
+                                <th scope="col">{{ __('fair no') }}</th>
                                 <th scope="col">{{ __('Arabic Name') }}</th>
                                 <th scope="col">{{ __('English Name') }}</th>
-                                <th scope="col">{{ __('Active') }}</th>
-                                <th scope="col">{{ __('Arabic Description') }}</th>
-                                <th scope="col">{{ __('English Description') }}</th>
+                                <th scope="col">{{ __('hits') }}</th>
                                 <th scope="col">{{ __('imgfile') }}</th>
                                 <th scope="col" width="150">{{ __('Control') }}</th>
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($fair->categories as $category)
+                            @foreach ($categories as $category)
                             <tr>
                                 <th scope="row">
-                                    {{-- <a href="{{ route('category.edit', $category->id) }}"><span class="iconify"
+                                    <a href="{{ route('category.edit', $category->id) }}"><span class="iconify"
                                         data-icon="feather:edit" data-inline="false" height="36" width="36">
-                                        {{ $category->id }}</span></a> --}}
+                                   {{ $category->id }}</span></a>
                                 </th>
-                                <td>{{ $category->name }}</td>
+                                <td>{{ $category->fair_id }}</td>
+                                <td>{{ $category->name_ar }}</td>
                                 <td>{{ $category->name_en }}</td>
-                                <td>{{ $category->active }}</td>
-                                <td>{{ $category->description }}</td>
-                                <td>{{ $category->description_en }}</td>
+                                 <td>{{ $category->hits }}</td>
                                 <td><img src="{{ asset('storage/categories/' . $category->imgfile) }}" width="25%"
                                         class="img-fluid img-thumbnail"></td>
                                 <td>
                                     <div class="btn-group-justified">
-                                        <div class="btn-group">
+                                        {{-- <div class="btn-group">
                                             <a class="btn btn-outline-warning rounded-circle"
                                                 href="{{route('category.edit',$category->id)}}">
                                                 <i class="zmdi zmdi-settings"></i>
                                             </a>
-                                        </div>
+                                        </div> --}}
                                         <div class="btn-group">
                                             <form action="{{ route('category.destroy', $category->id) }}" method="POST">
                                                 @csrf
